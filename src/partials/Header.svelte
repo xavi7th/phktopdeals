cb412b7ff8ecb2feca8d25663c2f796ba4f45b51
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-08-26 18:15:51 +0100

    Scafold Nav

---
+++ src/partials/Header.svelte
@@ -0,0 +1,183 @@
+<header
+	class="fixed z-10 flex w-full flex-wrap bg-black/40 py-4 text-sm backdrop-blur sm:flex-nowrap sm:justify-start
+            dark:bg-neutral-900 dark:lg:bg-transparent"
+>
+	<nav
+		class="mx-auto flex w-full max-w-[85rem] basis-full flex-wrap items-center justify-between px-4"
+	>
+		<a
+			class="flex-none text-xl font-semibold focus:opacity-80 focus:outline-none sm:order-1 dark:text-white"
+			href="/"
+		>
+			<img
+				class="rounded-full"
+				src="https://images.unsplash.com/photo-1612810806563-4cb8265db55f?q=80&w=500&h=500&auto=format&fit=crop"
+				alt="Logo"
+				width="50"
+			/>
+		</a>
+
+		<div class="flex items-center gap-x-2 sm:order-4 sm:hidden">
+			<button
+				type="button"
+				class="hs-collapse-toggle relative flex size-7 items-center justify-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:bg-gray-50 focus:outline-none disabled:pointer-events-none disabled:opacity-50 sm:hidden dark:border-neutral-700 dark:bg-transparent dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+				id="hs-navbar-alignment-collapse"
+				aria-expanded="false"
+				aria-controls="hs-navbar-alignment"
+				aria-label="Toggle navigation"
+				data-hs-collapse="#hs-navbar-alignment"
+			>
+				<svg
+					class="size-4 shrink-0 hs-collapse-open:hidden"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line
+						x1="3"
+						x2="21"
+						y1="18"
+						y2="18"
+					/></svg
+				>
+				<svg
+					class="hidden size-4 shrink-0 hs-collapse-open:block"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
+				>
+				<span class="sr-only">Toggle</span>
+			</button>
+			<button
+				type="button"
+				class="mr-1 inline-flex items-center gap-x-3 rounded-full bg-gray-800 px-5 py-3 text-xs font-medium text-white hover:bg-gray-900 focus:bg-gray-900 focus:outline-none dark:bg-white dark:text-neutral-800"
+			>
+				Sign In
+			</button>
+		</div>
+
+		<div
+			id="hs-navbar-alignment"
+			class="hs-collapse hidden grow basis-full space-x-0.5 overflow-hidden rounded-full border border-gray-200 transition-all duration-300 sm:order-2
+      sm:block sm:grow-0 sm:basis-auto dark:border-white/20 bg-gray-200"
+			aria-labelledby="hs-navbar-alignment-collapse"
+		>
+			<div class="mt-5 flex flex-col gap-5 sm:mt-0 sm:flex-row sm:items-center">
+				<a class="p-3.5 font-medium text-blue-500 focus:outline-none ml-4" href="#" aria-current="page"
+					>Landing</a
+				>
+				<a
+					class="p-3.5 font-medium text-gray-600 hover:text-gray-400 focus:text-gray-400 focus:outline-none dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+					href="#">Account</a
+				>
+				<a
+					class="p-3.5 font-medium text-gray-600 hover:text-gray-400 focus:text-gray-400 focus:outline-none dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+					href="#">Work</a
+				>
+				<a
+					class="mr-1 inline-flex items-center gap-x-3 rounded-full bg-gray-800 px-5 py-3 text-xs font-medium text-white hover:bg-gray-900 focus:bg-gray-900 focus:outline-none dark:bg-white dark:text-neutral-800"
+					href="#">Sign In</a
+				>
+			</div>
+		</div>
+
+		<div
+			class="hidden items-center rounded-full border border-gray-200 p-0.5 sm:order-3 sm:flex dark:border-white/20 bg-gray-200"
+		>
+			<button
+				type="button"
+				class="hs-dark-mode inline-flex items-center justify-center size-[46px] rounded-full font-medium text-gray-800
+                hover:bg-white/75 focus:bg-white/75 focus:outline-none hs-dark-mode-active:hidden
+                dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 ml-6"
+				data-hs-theme-click-value="dark"
+			>
+				<span class="group inline-flex size-9 shrink-0 items-center justify-center">
+					<svg
+						class="size-4 shrink-0"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+					</svg>
+				</span>
+			</button>
+
+      <button
+				type="button"
+				class="hs-dark-mode hidden rounded-full font-medium text-gray-800 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none
+            hs-dark-mode-active:block dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 ml-6"
+				data-hs-theme-click-value="light"
+			>
+				<span class="group inline-flex size-9 shrink-0 items-center justify-center">
+					<svg
+						class="size-4 shrink-0"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<circle cx="12" cy="12" r="4"></circle>
+						<path d="M12 2v2"></path>
+						<path d="M12 20v2"></path>
+						<path d="m4.93 4.93 1.41 1.41"></path>
+						<path d="m17.66 17.66 1.41 1.41"></path>
+						<path d="M2 12h2"></path>
+						<path d="M20 12h2"></path>
+						<path d="m6.34 17.66-1.41 1.41"></path>
+						<path d="m19.07 4.93-1.41 1.41"></path>
+					</svg>
+				</span>
+			</button>
+
+			<button
+				type="button"
+				class="relative mr-6 inline-flex size-[46px] items-center justify-center rounded-lg text-sm font-semibold
+        text-gray-800 shadow-sm hover:bg-white/75 focus:bg-white/75 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:bg-neutral-900
+        dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+			>
+				<svg
+					class="size-5 shrink-0"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"></path>
+					<path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"></path>
+				</svg>
+				<span
+					class="absolute end-0 top-0 inline-flex -translate-y-1/2 translate-x-1/2 transform items-center rounded-full bg-red-500 px-1.5 py-0.5 text-xs font-medium text-white"
+					>99+</span
+				>
+			</button>
+		</div>
+	</nav>
+</header>
