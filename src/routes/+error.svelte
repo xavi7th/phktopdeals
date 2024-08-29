db4136bc87eced3810e652986e58e79632ac5678
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-08-29 10:38:39 +0100

    Add error pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,65 +1,201 @@
 <script>
   import { page } from '$app/stores'
+	import { onMount } from 'svelte';
 
   $: console. log ('page', $page);
+
+  onMount(() => {
+    let PAGE_NOT_FOUND = document.querySelector(".caution__tape .PAGE_NOT_FOUND")
+    let tape__center = document.getElementById("caution__tape__center")
+    let tape__left = document.getElementById("caution__tape__left")
+    let body = document.querySelector("body")
+    let ERROR = document.querySelector(".caution__tape .ERROR")
+    let body_Width = body.clientWidth
+
+    body?.classList.add('error-page')
+
+    let warning_len = ((body_Width / PAGE_NOT_FOUND.clientWidth) / 1.8) - 1;
+    let caution_len = ((body_Width / ERROR.clientWidth) / 1.8) - 1;
+
+    for (let i = 0; i < warning_len; i++) {
+        tape__center.innerHTML += PAGE_NOT_FOUND.outerHTML;
+    }
+
+    for (let i = 0; i < caution_len; i++) {
+        tape__left.innerHTML += ERROR.outerHTML;
+    }
+
+    window.addEventListener("deviceorientation",function (e) {
+        tape__left.style.transform = "translate(" + (e.gamma /3) +"px ," + (e.beta/3) + "px) rotateZ(-45deg)"
+        tape__center.style.transform = "translate(" + (e.gamma/2) +"px ," + (e.beta/2) + "px) scale(1.5) rotateZ(15deg)"
+    } )
+
+    window.addEventListener("mousemove",function (e) {
+        tape__left.style.transform = "translate(" + e.pageX /30 +"px ," +e.pageY/30 + "px) rotateZ(-45deg)"
+        tape__center.style.transform = "translate(" + e.pageX /10 +"px ," +e.pageY/10 + "px) scale(1.5) rotateZ(15deg)"
+    })
+  })
 </script>
 
-<main class="error-page py-40">
-	<div class="container flex flex-col items-center text-center gap-y-16">
-		<div class="eyes flex justify-center gap-0.5">
-			<div class="eye w-20 h-20 bg-yellow-500 dark:bg-brand rounded-[50%] grid place-items-center">
-				<div class="w-6 h-6 bg-gray-700 rounded-[50%] eye-pupil pupil-left"></div>
-			</div>
-			<div class="eye w-20 h-20 bg-yellow-500 dark:bg-brand rounded-[50%] grid place-items-center">
-				<div class="w-6 h-6 bg-gray-700 rounded-[50%] eye-pupil pupil-right"></div>
-			</div>
-		</div>
+<main class="main">
+  <h1 class="font-bold">{ ($page.status + '').split('').shift() }</h1>
+  <h1 class="X">
+      <span class="caution__tape text"></span>
+      <span class="caution__tape text"></span>
+  </h1>
+  <h1 class="font-bold">{ ($page.status + '').split('').pop() }</h1>
 
-		<div>
-			<h1 class="capitalise text-4xl font-semibold text-yellow-500 dark:text-brand">
-        {#if $page.status == 404}
-          Looks like you're lost
-        {:else}
-          {$page.error?.message}
-        {/if}
-      </h1>
-			<p class="mt-2.5 text-md text-black dark:text-white">{$page.status} error</p>
-		</div>
-
-		<a class="cta hover:bg-brand-400 border-2 border-sm border-brand-400 text-black hover:text-white hover:font-semibold dark:text-white py-4 px-8 border-md shadow-md shadow-brand-500 transition capitalize rounded-xl" href="/" aria-label="back to home" title="back to home">
-      back to home
-    </a>
-	</div>
 </main>
+<section class="background relative">
+  <a data-sveltekit-reload class="absolute top-[20rem] left-[37vw] sm:left-[42.5vw] bg-gray-900 hover:bg-brand-100 border-2 border-md border-gray-900 text-white hover:text-black font-bold py-4 px-8 transition capitalize rounded-xl" href="/" aria-label="go back home" title="go back home">
+    go back home
+  </a>
+  <div id="caution__tape__left" class="caution__tape left">
+      <p class="ERROR">Oops!</p>
+  </div>
+  <div id="caution__tape__center" class="caution__tape center">
+      <p class="PAGE_NOT_FOUND">{ $page.error?.message }!</p>
+  </div>
+</section>
 
 <style lang="scss">
-  .cta {
-    box-shadow: 0 7px 0 -2px $themePrimary;
-  }
+:global{
+  .error-page {
+    font-family: 'Poppins', sans-serif;
+    background-color: #dfaa4eff;
+    background-image: linear-gradient(147deg, #fdaa4eff 25%, #fac07e 50%, #dfaa4eff 80%);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
 
-  .eye-pupil{
-    animation: movePupil 2s infinite ease-in-out;
-    transform-origin: center center;
-
-    &.pupil-right{
-      // To reverse the animation of the right eye, uncomment this.
-      // animation-direction: reverse;
+    header{
+      align-self: flex-start;
     }
 
-    @keyframes movePupil {
-      0%,
-      100% {
-        transform: translate(0, 0);
-      }
-      25% {
-        transform: translate(-10px, -10px);
-      }
-      50% {
-        transform: translate(10px, 10px);
-      }
-      75% {
-        transform: translate(-10px, 10px);
+    footer{
+      position: fixed !important;
+      bottom: -280px;
+
+      @media (max-width: 991px) {
+        display: none;
       }
     }
   }
+
+}
+
+.cta{
+  // position: absolute;
+  // top: 20rem;
+  // left: 42.5vw;
+}
+
+.main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.main h1 {
+  font-size: 10em;
+}
+
+.main h1.X {
+  width: 110px;
+  margin: 0 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.caution__tape.text {
+  width: 150vw;
+  height: 10px;
+}
+
+.caution__tape.text:nth-child(1) {
+  transform: rotateZ(45deg);
+}
+
+
+.caution__tape.text:nth-child(2) {
+  transform: rotateZ(-45deg);
+}
+
+.background {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+}
+
+.caution__tape {
+  position: absolute;
+  z-index: 3;
+  width: 105%;
+  height: 40px;
+  user-select: none;
+  background-color: #f8b930;
+  background-image: linear-gradient(#ffb700 0%, #ffc600 100%);
+  color: #0a0a0a;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  font-weight: bolder;
+  box-shadow: 0 0 15px rgba(10, 10, 10, 0.3);
+  border: 1px rgba(0, 0, 0, 0.9) solid;
+}
+
+.caution__tape.center {
+  bottom: 30%;
+  transform: scale(1.5) rotateZ(15deg);
+  -webkit-animation: show_tape_center 500ms cubic-bezier(0.18, 0.89, 0.32, 1.28) both;
+  -o-animation: show_tape_center 500ms cubic-bezier(0.18, 0.89, 0.32, 1.28) both;
+  animation: show_tape_center 500ms cubic-bezier(0.18, 0.89, 0.32, 1.28) both;
+}
+
+.caution__tape.left {
+  top: 10%;
+  left: -20%;
+  transform: rotateZ(-45deg);
+  -webkit-animation: show_tape_left 500ms cubic-bezier(0.18, 0.89, 0.32, 1.28) both;
+  -o-animation: show_tape_left 500ms cubic-bezier(0.18, 0.89, 0.32, 1.28) both;
+  animation: show_tape_left 500ms cubic-bezier(0.18, 0.89, 0.32, 1.28) both;
+}
+
+@keyframes show_tape_left {
+  0% {
+      top: 0;
+  }
+}
+
+@keyframes show_tape_center {
+  0% {
+      bottom: 0;
+  }
+}
+
+@media (max-height: 320px) {
+  .caution__tape.center {
+      display: none;
+  }
+}
+
+@media (max-width: 768px) {
+  main h1 {
+      font-size: 7em;
+  }
+.caution__tape{
+  width: 130%;
+}
+.caution__tape.left {
+  top: 10%;
+  left: -35%;
+}
+}
 </style>
---
+++ src/routes/error-bkp.svelte
@@ -0,0 +1,65 @@
+<script>
+  import { page } from '$app/stores'
+
+  $: console. log ('page', $page);
+</script>
+
+<main class="error-page py-40">
+	<div class="container flex flex-col items-center text-center gap-y-16">
+		<div class="eyes flex justify-center gap-0.5">
+			<div class="eye w-20 h-20 bg-yellow-500 dark:bg-brand rounded-[50%] grid place-items-center">
+				<div class="w-6 h-6 bg-gray-700 rounded-[50%] eye-pupil pupil-left"></div>
+			</div>
+			<div class="eye w-20 h-20 bg-yellow-500 dark:bg-brand rounded-[50%] grid place-items-center">
+				<div class="w-6 h-6 bg-gray-700 rounded-[50%] eye-pupil pupil-right"></div>
+			</div>
+		</div>
+
+		<div>
+			<h1 class="capitalise text-4xl font-semibold text-yellow-500 dark:text-brand">
+        {#if $page.status == 404}
+          Looks like you're lost
+        {:else}
+          {$page.error?.message}
+        {/if}
+      </h1>
+			<p class="mt-2.5 text-md text-black dark:text-white">{$page.status} error</p>
+		</div>
+
+		<a class="cta hover:bg-brand-400 border-2 border-sm border-brand-400 text-black hover:text-white hover:font-semibold dark:text-white py-4 px-8 border-md shadow-md shadow-brand-500 transition capitalize rounded-xl" href="/" aria-label="back to home" title="back to home">
+      back to home
+    </a>
+	</div>
+</main>
+
+<style lang="scss">
+  .cta {
+    box-shadow: 0 7px 0 -2px $themePrimary;
+  }
+
+  .eye-pupil{
+    animation: movePupil 2s infinite ease-in-out;
+    transform-origin: center center;
+
+    &.pupil-right{
+      // To reverse the animation of the right eye, uncomment this.
+      // animation-direction: reverse;
+    }
+
+    @keyframes movePupil {
+      0%,
+      100% {
+        transform: translate(0, 0);
+      }
+      25% {
+        transform: translate(-10px, -10px);
+      }
+      50% {
+        transform: translate(10px, 10px);
+      }
+      75% {
+        transform: translate(-10px, 10px);
+      }
+    }
+  }
+</style>
