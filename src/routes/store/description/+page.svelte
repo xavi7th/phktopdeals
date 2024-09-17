e38e93ae6422115d75ea9719feb0a1127d7bac9b
Ranger <oghale.b.emeselue@gmail.com>
2024-09-17 15:17:18 +0100

    update: description design

--- src/routes/store/description/+page.svelte
+++ src/routes/store/description/+page.svelte
@@ -12,69 +12,148 @@
         "optionTemplate": "<div><div class='flex items-center'><div class='me-2' data-icon></div><div class='text-gray-800 dark:text-neutral-200' data-title></div></div></div>",
         "extraMarkup": "<div class='absolute top-1/2 end-3 -translate-y-1/2'><svg class='shrink-0 size-3.5 text-gray-500 dark:text-neutral-500' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='m7 15 5 5 5-5'/><path d='m7 9 5-5 5 5'/></svg></div>"
     }`;
+
+    let count = 0;
 </script>
 
 <div class="container py-28 lg:py-40 px-4">
     <div>
       <main class="grid sm:grid-cols-2 grid-cols-1 gap-4 mb-8">
-        <div class="w-full px-40">
+        <div class="w-full pr-20">
             <div class="">
                 <enhanced:img class="w-full h-auto rounded-xl" src="$lib/images/games-6.avif?aspect=400:320&fit=cover" alt="hero-img-thumb" />
             </div>
         </div>
-        <div class="w-full bg-green-400">
+        <div>
+          <div class="w-full">
             <div>
-                <div class="title font-black" style="font-size: 40px; font-weight: bold;">The Title</div>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi maiores deleniti ullam distinctio velit tenetur quo, dolorem reiciendis. Consequatur placeat ipsum corrupti optio, iste delectus. Veniam modi laborum odio alias? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi maiores deleniti ullam distinctio velit tenetur quo, dolorem reiciendis. Consequatur placeat ipsum corrupti optio, iste delectus. Veniam modi laborum odio alias?</p>
-                <br>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi maiores deleniti ullam distinctio velit tenetur quo, dolorem reiciendis. Consequatur placeat ipsum corrupti optio, iste delectus. Veniam modi laborum odio alias?</p>
+                <div class="title text-black dark:text-white" style="font-size: 40px; font-weight: bold;">The Title</div>
             </div>
+          </div>
+          <div class="bg-brand-100 p-4 rounded-lg mt-5">
+            <div class="flex flex-col sm:gap-4 gap-2">
+              <div class="relative">
+                <div class="flex items-center h-full w-[100px] absolute top-[50%] translate-y-[-50%] left-4 rounded-lg overflow-hidden">
+                  <p>Your Email: </p>
+                </div>
+                <input type="text" class="py-3 px-4 pl-[100px] block w-full border-gray-200 rounded-lg text-sm focus:border-brand-500 focus:ring-brand-500 disabled:opacity-50
+                disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                placeholder="Your Email">
+              </div>
+              <div class="relative">
+                <div class="flex items-center h-full w-[70px] absolute top-[50%] translate-y-[-50%] left-4 rounded-lg overflow-hidden">
+                  <p>Count: </p>
+                </div>
+                <input type="number" class="py-3 pl-[67px] px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-brand-500 focus:ring-brand-500 disabled:opacity-50
+                disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                placeholder="Count" bind:value={count}>
+                <div class="flex h-[35px] w-[70px] absolute top-[50%] translate-y-[-50%] right-2 rounded-lg overflow-hidden">
+                  <button type="button" class="px-2 h-full inline-flex items-center font-medium border border-transparent bg-brand text-brand-800 hover:bg-brand-700 hover:text-brand-50 focus:outline-none focus:bg-brand-700 disabled:opacity-50 disabled:pointer-events-none" style="width: 200px; justify-content: center;" on:click={() => count++}>
+                    +
+                  </button>
+                  <button type="button" class="px-2 h-full inline-flex items-center font-medium border border-transparent bg-brand text-brand-800 hover:bg-brand-700 hover:text-brand-50 focus:outline-none focus:bg-brand-700 disabled:opacity-50 disabled:pointer-events-none" style="width: 200px; justify-content: center;" disabled={count <= 0 ? true : false} on:click={() => count--}>
+                    -
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bg-brand-100 p-4 rounded-t-lg mt-5">
+            <div class="flex sm:flex-row flex-col sm:gap-4 gap-2">
+              <select id="platform-select" data-hs-select={platformSelectOptions} class="hidden">
+                  <option value="">Choose</option>
+                  <option value="AF" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/af.png' alt='Af' />"}`}'>
+                    Steam
+                  </option>
+                  <option value="AX" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/ax.png' alt='AI' />"}`}'>
+                    EA Games
+                  </option>
+                  <option value="AL" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/al.png' alt='Al' />"}`}'>
+                    Battle.net
+                  </option>
+                  <option value="DZ" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/dz.png' alt='Alg' />"}`}'>
+                    Ubisoft
+                  </option>
+                  <option value="AS" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/as.png' alt='AS' />"}`}'>
+                    American Samoa
+                  </option>
+              </select>
+              <button type="button" class="py-1.5 px-5 inline-flex items-center font-medium rounded-lg border border-transparent bg-brand text-brand-800 hover:bg-brand-700 hover:text-brand-50 focus:outline-none focus:bg-brand-700 disabled:opacity-50 disabled:pointer-events-none" style="width: 200px; justify-content: center;">
+                  Add To Cart
+              </button>
+            </div>
+            <div class="py-2 md:py-3 flex-1 mt-4">
+  
+              <div class="flex flex-wrap items-start gap-1.5 border-t-2 pt-3">
+                {#each Array(10) as item}
+                  <span class="inline-flex items-center py-1 px-2 rounded-full text-sm bg-brand-200 text-brand-900 dark:bg-brand-800/30 dark:text-brand-400">10 USDT</span>
+                {/each}
+              </div>
+            </div>
+          </div>
+          <div class="bg-brand-100 p-4 flex gap-3 items-center">
+            <p>Payment Method:</p>
+            <select id="platform-select" data-hs-select={platformSelectOptions} class="hidden">
+              <option value="">Choose</option>
+              <option value="AF" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='https://cdn.pixabay.com/photo/2021/04/30/16/47/binance-logo-6219389_1280.png' alt='Af' />"}`}' selected>
+                Binance
+              </option>
+              <option value="AX" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/ax.png' alt='AI' />"}`}'>
+                EA Games
+              </option>
+              <option value="AL" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/al.png' alt='Al' />"}`}'>
+                Battle.net
+              </option>
+              <option value="DZ" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/dz.png' alt='Alg' />"}`}'>
+                Ubisoft
+              </option>
+              <option value="AS" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/as.png' alt='AS' />"}`}'>
+                American Samoa
+              </option>
+            </select>
+          </div>
+          <div class="bg-brand-100 p-4 flex flex-col gap-3 justify-center items-center">
+            <p class="text-left">The platform does not support single brushing or rebates. Please be cautious of fraud and do not fill in other people's top-up accounts to prevent scams.</p>
+            <button type="button" class="py-1.5 px-10 mt-10 inline-flex items-center font-medium rounded-lg border border-transparent bg-brand text-brand-800 hover:bg-brand-700 hover:text-brand-50 focus:outline-none focus:bg-brand-700 disabled:opacity-50 disabled:pointer-events-none" style="justify-content: center;">
+              Buy Now $ 4,000
+            </button>
+          </div>
         </div>
       </main>
     </div>
     <div class="bg-brand-100 p-4 rounded-lg">
-        <div class="flex sm:flex-row flex-col sm:gap-4 gap-2">
-            <div class="space-y-3 relative w-full">
-                <input type="text" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-brand-500 focus:ring-brand-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search products">
-            
-                <div class="absolute inset-y-0 end-0 !mt-0 flex items-center pointer-events-none pe-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-                  <svg class="shrink-0 size-4 text-gray-500 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M11.36 20.213l-2.36 .787v-8.5l-4.48 -4.928a2 2 0 0 1 -.52 -1.345v-2.227h16v2.172a2 2 0 0 1 -.586 1.414l-4.414 4.414" />
-                    <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                    <path d="M20.2 20.2l1.8 1.8" />
-                  </svg>
+        <div>
+          <div>
+            <div>
+              <div>
+                <div class="py-4">
+                  <span class="text-2xl font-bold">Product Description</span>
                 </div>
+                <div style="transform: translateX(165px) translateX(-50%); transition-duration: 0.3s;"></div>
+              </div>
             </div>
-            <select id="platform-select" data-hs-select={platformSelectOptions} class="hidden">
-                <option value="">Choose</option>
-                <option value="AF" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/af.png' alt='Af' />"}`}'>
-                  Steam
-                </option>
-                <option value="AX" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/ax.png' alt='AI' />"}`}'>
-                  EA Games
-                </option>
-                <option value="AL" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/al.png' alt='Al' />"}`}'>
-                  Battle.net
-                </option>
-                <option value="DZ" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/dz.png' alt='Alg' />"}`}'>
-                  Ubisoft
-                </option>
-                <option value="AS" data-hs-select-option='{`{"icon": "<img class='inline-block size-4 rounded-full' src='../assets/vendor/svg-country-flags/png100px/as.png' alt='AS' />"}`}'>
-                  American Samoa
-                </option>
-            </select>
-            <button type="button" class="py-1.5 px-5 inline-flex items-center font-medium rounded-lg border border-transparent bg-brand text-brand-800 hover:bg-brand-700 hover:text-brand-50 focus:outline-none focus:bg-brand-700 disabled:opacity-50 disabled:pointer-events-none" style="width: 200px; justify-content: center;">
-                Add To Cart
-            </button>
-        </div>
-        <div class="py-2 md:py-3 flex-1 mt-4">
-
-            <div class="flex flex-wrap items-start gap-1.5 border-t-2 pt-3">
-              {#each Array(10) as item}
-                <span class="inline-flex items-center py-1 px-2 rounded-full text-sm bg-brand-200 text-brand-900 dark:bg-brand-800/30 dark:text-brand-400">10 USDT</span>
-              {/each}
+            <div>
+              <div>
+                <div class="question-item space-y-4">
+                  <!-- <h3 class="mb-4">Product Description</h3> -->
+                  <p>Amazon.com Gift Cards* never expire and can be redeemed towards millions of items at www.amazon.com</p>
+                  <h3>Redemption</h3>
+                  <p>To redeem your gift card, follow these steps:</p>
+                  <p>1.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Visit <a href="https://panel.ezpaypin.com/www.amazon.com/redeem" rel="noopener noreferrer" target="_blank" style="color: rgb(3, 155, 229);">www.amazon.com/redeem</a></p>
+                  <p>2.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter the Claim Code when prompted.</p>
+                  <p>3.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gift card funds will be applied automatically to eligible orders during the checkout process.</p>
+                  <p>4.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You must pay for any remaining balance on your order with another payment method.</p>
+                  <p>&nbsp;</p>
+                  <p>Your gift card claim code may also be entered when prompted during checkout. To redeem your gift card using the <a href="https://www.amazon.com/" rel="noopener noreferrer" target="_blank" style="color: rgb(3, 155, 229);">Amazon.com</a> 1-Click® service, first add the gift card funds to Your Account.</p>
+                  <p>If you have questions about redeeming your gift card, please visit <a href="https://panel.ezpaypin.com/www.amazon.com/gc-redeem" rel="noopener noreferrer" target="_blank" style="color: rgb(3, 155, 229);">www.amazon.com/gc-redeem</a>.</p>
+                  <h3>Terms and Conditions</h3>
+                  <p>Restrictions apply, see amazon.com/gc-legal</p>
+                  <h3>Legal Disclaimer</h3>
+                  <p>Restrictions apply, see <a href="https://panel.ezpaypin.com/amazon.com/gc-legal" rel="noopener noreferrer" target="_blank" style="color: rgb(3, 155, 229);">amazon.com/gc-legal</a></p>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
     </div>
 </div>
