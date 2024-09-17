6df08b138c442b985608cebe60eab87988f89d1e
Ranger <oghale.b.emeselue@gmail.com>
2024-09-17 16:56:39 +0100

    fix: button dark color

--- src/lib/Components/PrimaryBtn.svelte
+++ src/lib/Components/PrimaryBtn.svelte
@@ -2,6 +2,6 @@
     export let data;
 </script>
 
-<button class="bg-brand hover:bg-brand-400 rounded-full px-5 py-3 text-sm">
+<button class="bg-brand hover:bg-brand-400 dark:text-black rounded-full px-5 py-3 text-sm">
     <span class="">{data.innerText}</span>
 </button>
