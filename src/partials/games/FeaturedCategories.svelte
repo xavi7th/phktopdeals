93a04ec241f4c4f5be31a31010a1dd1945310604
Ranger <oghale.b.emeselue@gmail.com>
2024-10-02 16:56:23 +0100

    fix: make games page dynamic

--- src/partials/games/FeaturedCategories.svelte
+++ src/partials/games/FeaturedCategories.svelte
@@ -1,21 +1,23 @@
-
+<script>
+  export let category;
+</script>
 
 <main class="grid grid-cols-subgrid col-span-4 lg:grid-cols-5 grid-cols-5 gap-3 mb-8 px-2 flex-wrap">
 
-  {#each [1,2,3,4,5] as n}
+  {#each category as item, index}
     <a class="flex flex-col group bg-white shadow-sm rounded-xl overflow-hidden hover:shadow-lg focus:outline-none focus:shadow-lg transition dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70" href="/">
       <div class="relative bg-white shadow-md rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 overflow-hidden">
-        {#if (n%2 == 0)}
-          <enhanced:img class="w-full h-56 rounded-xl group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out" src="$lib/images/games-1.avif?enhance&fit=cover" alt="card-thumb" />
-        {:else if (n%3 == 0)}
-          <enhanced:img class="w-full h-56 rounded-xl group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out" src="$lib/images/games-3.avif?enhance&fit=cover" alt="card-thumb" />
+        {#if (index%2 == 0)}
+          <img class="w-full h-56 rounded-xl group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out" src={item.imgUrl} alt="card-thumb" />
+        {:else if (index%3 == 0)}
+          <img class="w-full h-56 rounded-xl group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out" src={item.imgUrl} alt="card-thumb" />
         {:else}
-          <enhanced:img class="w-full h-56 rounded-xl group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out" src="$lib/images/games-2.avif?enhance&fit=cover" alt="card-thumb" />
+          <img class="w-full h-56 rounded-xl group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out" src={item.imgUrl} alt="card-thumb" />
         {/if}
 
         <div class="absolute bottom-0 start-0 end-0">
           <div class="bg-black/70 group-hover:bg-brand-400 rounded-b-xl py-3 px-4 md:py-4 md:px-5 text-white group-hover:text-black dark:bg-neutral-900/90 dark:border-neutral-700 text-gray-500 dark:text-neutral-500 font-semibold text-center">
-            DLC Games
+            {item.title}
           </div>
         </div>
       </div>
--- src/routes/api/home/data.js
+++ src/routes/api/home/data.js
@@ -79,6 +79,104 @@ export const prod_desc = {
   `,
 };
 
+export const games = {
+  category: [
+    {
+      title: "DLC",
+      imgUrl: "https://cdn.mos.cms.futurecdn.net/7Cbpe8JbsPYc3ZfhzrP5ae-1200-80.jpg",
+    },
+  ],
+  list: [
+    {
+      name: 'Assassin\'s Creed Brotherhood PC Ubisoft Connect CD Key',
+      product_type: 'Games',
+      url: '/store/gift-cards/itunes-card',
+      imgUrl: 'https://static.kinguin.net/media/images/products/_AssassinsCreedBrotherhood111.jpg',
+      country: 'US', //, 'Global', 'Global Except Latin America'
+      brand: 'Amazon', //Rewarble,
+      categories: ['entertainment', 'games', 'software'],
+      amount: 20.45,
+      price_tags: { // All currencies will be set in dollars. We can implement a site-wide converter later OR we can have a field that specifies currencies (more difficult)
+        min: 14.02,
+        denominations: [14.02, 22.40, 37, 55.30],
+        flexible: true, //determines whether they will be able to enter their own price. On create there will be a checkbox that determines if others will be added or not.
+        commission: 5 // a percentage that determines how much markup will be to the total purchase as business profit
+      },
+      discount: 5, //percent off or false. If there is a discount display like this 9.5U will get you $10 (Assuming a 5% discount)
+      discount_until: 3, //Number of days left for discount to expire. This may not be sent to front end. discount becomes false after this date. Those that match today come under today's deals
+      faq: `
+        <div class="mat-expansion-panel-body ng-tns-c857250080-1035"><div _ngcontent-ng-c3917189997="" class="text-bolder ng-star-inserted" style=""><span _ngcontent-ng-c3917189997="" class="text-xl font-bold"> Description </span></div><div _ngcontent-ng-c3917189997="" class="mb-4 ng-star-inserted" style="">Live and breathe as Ezio, a legendary Master Assassin, in his enduring struggle against the powerful Templar Order. He must journey into Italy’s greatest city, Rome, center of power, greed and corruption to strike at the heart of the enemy. Defeating the corrupt tyrants entrenched there will require not only strength, but leadership, as Ezio commands an entire Brotherhood who will rally to his side. Only by working together can the Assassins defeat their mortal enemies. And for the first time, introducing a never-before-seen multiplayer layer that allows you to choose from a wide range of unique characters, each with their own signature weapons and assassination techniques, and match your skills against other players from around the world. It’s time to join the Brotherhood.</div><!----><!----><!----><div _ngcontent-ng-c3917189997="" class="font-bolder ng-star-inserted" style=""><span _ngcontent-ng-c3917189997="" class="text-xl font-bold"> Activation Details </span></div><div _ngcontent-ng-c3917189997="" class="mb-4 ng-star-inserted" style="">Go to:  http://uplay.ubi.com and download Uplay client.
+        Install Uplay Client on your PC.
+        Start the application, login with your Account name and Password (create one if you don't have).
+        Please follow these instructions to activate a new retail purchase on Uplay:
+        Click the Settings
+        Choose Activate Product
+        Follow the onscreen instructions to complete the process.</div><!----><!----><!----><!----></div>
+    
+        <div data-v-b25dd4ac="" class="question-item"><h3>Redemption Instruction:</h3><p><br></p><p>Redeem your iTunes card via the App Store, or via the Apple Music app on your iPhone.</p><p>In the App Store:</p><p>Log in with your Apple ID and navigate to 'Redeem';</p><p>Enter the code and the balance is topped up Immediately;</p><p>You're done, enjoy!</p><p>Via the Apple Music App</p><p>Open the iTunes App;</p><p>Click on 'Redeem' and enter the 16 digit code;</p><p>Your balance has been topped up! That's it, you are now ready to get started with the latest games and apps.</p><h3>Product Description</h3><p>One card, millions of ways to enjoy it. Use the App Store &amp; iTunes Gift Card to get apps, games, music, films, and TV programmers. Available in a variety of denominations—spend it on in-app content, books, TV-programmer subscriptions, or even iCloud storage to secure files from all your Apple devices</p><p><br></p><h3>Terms and Conditions:</h3><p>Valid only on purchases from the US iTunes Store. Use requires an active iTunes account &amp; prior acceptance of license &amp; usage terms.</p><p>Brand Partner Contact:</p><p><br></p><p>Visit:<a href="https://www.apple.com/" rel="noopener noreferrer" target="_blank" style="color: rgb(3, 155, 229);">https://www.apple.com</a></p></div>
+      `,
+    },
+    {
+      name: 'Assassin\'s Creed Brotherhood PC Ubisoft Connect CD Key',
+      product_type: 'Games',
+      url: '/store/gift-cards/itunes-card',
+      imgUrl: 'https://static.kinguin.net/media/images/products/_AssassinsCreedBrotherhood111.jpg',
+      country: 'US', //, 'Global', 'Global Except Latin America'
+      brand: 'Amazon', //Rewarble,
+      amount: 20.45,
+      categories: ['entertainment', 'games', 'software'],
+      price_tags: { // All currencies will be set in dollars. We can implement a site-wide converter later OR we can have a field that specifies currencies (more difficult)
+        min: 14.02,
+        denominations: [14.02, 22.40, 37, 55.30],
+        flexible: true, //determines whether they will be able to enter their own price. On create there will be a checkbox that determines if others will be added or not.
+        commission: 5 // a percentage that determines how much markup will be to the total purchase as business profit
+      },
+      discount: 5, //percent off or false. If there is a discount display like this 9.5U will get you $10 (Assuming a 5% discount)
+      discount_until: 3, //Number of days left for discount to expire. This may not be sent to front end. discount becomes false after this date. Those that match today come under today's deals
+      faq: `
+        <div class="mat-expansion-panel-body ng-tns-c857250080-1035"><div _ngcontent-ng-c3917189997="" class="text-bolder ng-star-inserted" style=""><span _ngcontent-ng-c3917189997="" class="text-xl font-bold"> Description </span></div><div _ngcontent-ng-c3917189997="" class="mb-4 ng-star-inserted" style="">Live and breathe as Ezio, a legendary Master Assassin, in his enduring struggle against the powerful Templar Order. He must journey into Italy’s greatest city, Rome, center of power, greed and corruption to strike at the heart of the enemy. Defeating the corrupt tyrants entrenched there will require not only strength, but leadership, as Ezio commands an entire Brotherhood who will rally to his side. Only by working together can the Assassins defeat their mortal enemies. And for the first time, introducing a never-before-seen multiplayer layer that allows you to choose from a wide range of unique characters, each with their own signature weapons and assassination techniques, and match your skills against other players from around the world. It’s time to join the Brotherhood.</div><!----><!----><!----><div _ngcontent-ng-c3917189997="" class="font-bolder ng-star-inserted" style=""><span _ngcontent-ng-c3917189997="" class="text-xl font-bold"> Activation Details </span></div><div _ngcontent-ng-c3917189997="" class="mb-4 ng-star-inserted" style="">Go to:  http://uplay.ubi.com and download Uplay client.
+        Install Uplay Client on your PC.
+        Start the application, login with your Account name and Password (create one if you don't have).
+        Please follow these instructions to activate a new retail purchase on Uplay:
+        Click the Settings
+        Choose Activate Product
+        Follow the onscreen instructions to complete the process.</div><!----><!----><!----><!----></div>
+    
+        <div data-v-b25dd4ac="" class="question-item"><h3>Redemption Instruction:</h3><p><br></p><p>Redeem your iTunes card via the App Store, or via the Apple Music app on your iPhone.</p><p>In the App Store:</p><p>Log in with your Apple ID and navigate to 'Redeem';</p><p>Enter the code and the balance is topped up Immediately;</p><p>You're done, enjoy!</p><p>Via the Apple Music App</p><p>Open the iTunes App;</p><p>Click on 'Redeem' and enter the 16 digit code;</p><p>Your balance has been topped up! That's it, you are now ready to get started with the latest games and apps.</p><h3>Product Description</h3><p>One card, millions of ways to enjoy it. Use the App Store &amp; iTunes Gift Card to get apps, games, music, films, and TV programmers. Available in a variety of denominations—spend it on in-app content, books, TV-programmer subscriptions, or even iCloud storage to secure files from all your Apple devices</p><p><br></p><h3>Terms and Conditions:</h3><p>Valid only on purchases from the US iTunes Store. Use requires an active iTunes account &amp; prior acceptance of license &amp; usage terms.</p><p>Brand Partner Contact:</p><p><br></p><p>Visit:<a href="https://www.apple.com/" rel="noopener noreferrer" target="_blank" style="color: rgb(3, 155, 229);">https://www.apple.com</a></p></div>
+      `,
+    },
+    {
+      name: 'Assassin\'s Creed Brotherhood PC Ubisoft Connect CD Key',
+      product_type: 'Games',
+      url: '/store/gift-cards/itunes-card',
+      imgUrl: 'https://static.kinguin.net/media/images/products/_AssassinsCreedBrotherhood111.jpg',
+      country: 'US', //, 'Global', 'Global Except Latin America'
+      brand: 'Amazon', //Rewarble,
+      amount: 20.45,
+      categories: ['entertainment', 'games', 'software'],
+      price_tags: { // All currencies will be set in dollars. We can implement a site-wide converter later OR we can have a field that specifies currencies (more difficult)
+        min: 14.02,
+        denominations: [14.02, 22.40, 37, 55.30],
+        flexible: true, //determines whether they will be able to enter their own price. On create there will be a checkbox that determines if others will be added or not.
+        commission: 5 // a percentage that determines how much markup will be to the total purchase as business profit
+      },
+      discount: 5, //percent off or false. If there is a discount display like this 9.5U will get you $10 (Assuming a 5% discount)
+      discount_until: 3, //Number of days left for discount to expire. This may not be sent to front end. discount becomes false after this date. Those that match today come under today's deals
+      faq: `
+        <div class="mat-expansion-panel-body ng-tns-c857250080-1035"><div _ngcontent-ng-c3917189997="" class="text-bolder ng-star-inserted" style=""><span _ngcontent-ng-c3917189997="" class="text-xl font-bold"> Description </span></div><div _ngcontent-ng-c3917189997="" class="mb-4 ng-star-inserted" style="">Live and breathe as Ezio, a legendary Master Assassin, in his enduring struggle against the powerful Templar Order. He must journey into Italy’s greatest city, Rome, center of power, greed and corruption to strike at the heart of the enemy. Defeating the corrupt tyrants entrenched there will require not only strength, but leadership, as Ezio commands an entire Brotherhood who will rally to his side. Only by working together can the Assassins defeat their mortal enemies. And for the first time, introducing a never-before-seen multiplayer layer that allows you to choose from a wide range of unique characters, each with their own signature weapons and assassination techniques, and match your skills against other players from around the world. It’s time to join the Brotherhood.</div><!----><!----><!----><div _ngcontent-ng-c3917189997="" class="font-bolder ng-star-inserted" style=""><span _ngcontent-ng-c3917189997="" class="text-xl font-bold"> Activation Details </span></div><div _ngcontent-ng-c3917189997="" class="mb-4 ng-star-inserted" style="">Go to:  http://uplay.ubi.com and download Uplay client.
+        Install Uplay Client on your PC.
+        Start the application, login with your Account name and Password (create one if you don't have).
+        Please follow these instructions to activate a new retail purchase on Uplay:
+        Click the Settings
+        Choose Activate Product
+        Follow the onscreen instructions to complete the process.</div><!----><!----><!----><!----></div>
+    
+        <div data-v-b25dd4ac="" class="question-item"><h3>Redemption Instruction:</h3><p><br></p><p>Redeem your iTunes card via the App Store, or via the Apple Music app on your iPhone.</p><p>In the App Store:</p><p>Log in with your Apple ID and navigate to 'Redeem';</p><p>Enter the code and the balance is topped up Immediately;</p><p>You're done, enjoy!</p><p>Via the Apple Music App</p><p>Open the iTunes App;</p><p>Click on 'Redeem' and enter the 16 digit code;</p><p>Your balance has been topped up! That's it, you are now ready to get started with the latest games and apps.</p><h3>Product Description</h3><p>One card, millions of ways to enjoy it. Use the App Store &amp; iTunes Gift Card to get apps, games, music, films, and TV programmers. Available in a variety of denominations—spend it on in-app content, books, TV-programmer subscriptions, or even iCloud storage to secure files from all your Apple devices</p><p><br></p><h3>Terms and Conditions:</h3><p>Valid only on purchases from the US iTunes Store. Use requires an active iTunes account &amp; prior acceptance of license &amp; usage terms.</p><p>Brand Partner Contact:</p><p><br></p><p>Visit:<a href="https://www.apple.com/" rel="noopener noreferrer" target="_blank" style="color: rgb(3, 155, 229);">https://www.apple.com</a></p></div>
+      `,
+    }
+  ]
+}
+
 export const filters = [ // 3 random categories, brands, countries
   {
     filter: 'Amazon',
