e3888228bee73f1dce174c437ea17e9316ba9314
Ranger <43958691+MysticalProgrammer@users.noreply.github.com>
2024-10-04 11:08:30 +0100

    Update/e sim (#35)
    * update: e-sim page
    
    * update: Remove console.log statement in load function
    
    ---------
    
    Co-authored-by: Akhile E. Daniel <ehi.akhile@gmail.com>

--- src/routes/store/top-up/+page.svelte
+++ src/routes/store/top-up/+page.svelte
@@ -158,21 +158,25 @@
                                 <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 xl:gap-3 gap-5 mb-8">
                         
                                 {#each Array(10) as item}
-                                    <div class="group overflow-hidden relative flex flex-col bg-white shadow rounded-xl dark:bg-neutral-700/30 dark:border-neutral-700 dark:shadow-neutral-700/70 transition hover:drop-shadow-xl hover:shadow-sm hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025]">
-                                        <div class="px-4 md:px-5 py-2 md:py-2 flex-1">
-                                            <h3 class="text-lg text-center font-bold text-gray-800 dark:text-white pb-3 mb-5">VISA Card</h3>
-                                        </div>
-                            
-                                        <div class="flex items-end gap-1.5 transition-colors duration-300 bg-[#FFDA1C] hover:shadow-md shadow-[#FFDA1C] rounded-b-xl py-3 px-4 md:py-4 md:px-5 dark:border-brand-700 mt-1 text-sm text-black dark:text-brand-50 hover:text-white">
-                                            <div class="flex flex-col w-full gap-1">
-                                                <span class="text-md text-black">Amount</span>
-                                                <input type="text" class="border border-black/30 focus:border-[#FFDA1C] rounded-md">
-                                            </div>
-                                            <button type="button">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 48 48"><g fill="none" stroke="white" stroke-linejoin="round" stroke-width="4.25"><path d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path stroke-linecap="round" d="m21 33l9-9l-9-9"/></g></svg>
-                                            </button>
-                                        </div>
+                                <div class="group relative flex flex-col overflow-hidden rounded-2xl bg-white shadow transition hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-sm hover:drop-shadow-xl dark:border-neutral-700 dark:bg-neutral-700/30 dark:shadow-neutral-700/70">
+                                    <img class="h-auto w-full rounded-xl" src="{item.imgUrl}" alt="prod-thumb"/>
+                          
+                                    <div class="relative flex-1 px-4 py-2 md:px-5">
+                                      <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+                                        {item.name}
+                                      </h3>
                                     </div>
+                          
+                                    <div class="flex items-end gap-1.5 transition-colors duration-300 bg-[#FFDA1C] hover:shadow-md shadow-[#FFDA1C] rounded-b-xl py-3 px-4 md:py-4 md:px-5 dark:border-brand-700 mt-1 text-sm text-black dark:text-brand-50 hover:text-white">
+                                      <div class="flex flex-col gap-1 w-4/5">
+                                          <span class="text-md text-black">Amount</span>
+                                          <input type="text" class="border border-black/30 focus:border-[#FFDA1C] rounded-md">
+                                      </div>
+                                      <button type="button">
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 48 48"><g fill="none" stroke="white" stroke-linejoin="round" stroke-width="4.25"><path d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path stroke-linecap="round" d="m21 33l9-9l-9-9"/></g></svg>
+                                      </button>
+                                    </div>
+                                  </div>
                                 {/each}
                         
                                 </div>
