6e54d4c66f5664a19552cf0255a7caaa2cc77e10
Ranger <oghale.b.emeselue@gmail.com>
2024-10-28 10:23:09 +0100

    create: session storage in hook

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -45,6 +45,7 @@ async function getUserDetails({event, resolve}){
 
     if (getUserDetails?.status == 200) {
       //TODO: Set a localStorage with key user and expiration time for 5mins. If that key is present, no need to getUserDetails. @see https://www.sohamkamani.com/javascript/localstorage-with-ttl-expiry/
+      sessionStorage.setItem('user', JSON.stringify(getUserDetails?.json()));
       event.locals.user = await getUserDetails?.json() //use this to determine auth on frontend. Before accessing auth routes if this is null redirect to login page
     }
 	}
