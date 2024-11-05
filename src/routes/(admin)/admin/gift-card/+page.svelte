ec4c7a9437d02a4e99342ecc77c0ee4a0f242175
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-11-05 11:53:49 +0100

    Update gift cards page to show only gift cards

--- src/routes/(admin)/admin/gift-card/+page.svelte
+++ src/routes/(admin)/admin/gift-card/+page.svelte
@@ -1,7 +1,6 @@
 <script>
-	import { leftAngle, plusIcon, rightAngle } from '$lib/Components/iconPaths';
-	import SvgIcon from '$lib/Components/SvgIcon.svelte';
-	import { toCurrency } from '$lib/helpers';
+	import { plusIcon } from '$lib/Components/iconPaths';
+	import ProductListings from '../ProductListings.svelte';
 
   /** @type {import('./$types').PageData} */
   export let data
@@ -54,125 +53,7 @@
               </div>
             </div>
 
-            <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-              <thead class="bg-gray-50 dark:bg-neutral-800">
-                <tr>
-                  <th scope="col" class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3 text-start">
-                    <div class="flex items-center gap-x-2">
-                      <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                        Card Details
-                      </span>
-                    </div>
-                  </th>
-
-                  <th scope="col" class="px-6 py-3 text-start">
-                    <div class="flex items-center gap-x-2">
-                      <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                        Price Details
-                      </span>
-                    </div>
-                  </th>
-
-                  <th scope="col" class="px-6 py-3 text-start">
-                    <div class="flex items-center gap-x-2">
-                      <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                        Discount Details
-                      </span>
-                    </div>
-                  </th>
-                  <th scope="col" class="px-6 py-3 text-end"> Actions </th>
-                </tr>
-              </thead>
-
-              <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
-                {#each cards as card}
-                  <tr>
-                    <td class="size-px whitespace-nowrap">
-                      <div class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
-                        <div class="flex items-center gap-x-3">
-                          <img class="inline-block size-[38px] rounded-full" src="{card.product_image_url}" alt="Avatar" referrerpolicy="no-referrer">
-                          <div class="grow">
-                            <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-200">{card.product_name}</span>
-                            <span class="block text-sm text-gray-500 dark:text-neutral-500 text-wrap">{card.product_category.toString()}</span>
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="h-px w-72 whitespace-nowrap">
-                      <div class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
-                        <div class="flex items-center gap-x-3">
-                          <div class="grow">
-                            <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Commission Percentage: <span class="font-semibold">{ card.product_price.commission }%</span>
-                            <span class="block text-sm text-gray-500 dark:text-neutral-500 text-wrap">Denominations: <br />{ card.product_price.denominations?.map(x => toCurrency(x)) }</span></span>
-                            {#if card.product_price.flexible}
-                              <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Minimum Custom Price: { toCurrency(card.product_price.min) }</span>
-                            {/if}
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="size-px whitespace-nowrap">
-                      <div class="px-6 py-3">
-                        <span class="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium {card.percentage_discount ? 'bg-teal-100 text-teal-800 dark:bg-teal-500/10 dark:text-teal-500' : 'bg-gray-100 text-gray-800 dark:bg-gray-500/10 dark:text-gray-500'} rounded-full">
-                          <svg class="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                          </svg>
-                          {#if card.percentage_discount}
-                            {card.percentage_discount}% discount until {card.discount_until}
-                          {:else}
-                            None
-                          {/if}
-                        </span>
-                      </div>
-                    </td>
-                    <td class="size-px whitespace-nowrap">
-                      <div class="px-6 py-1.5 flex gap-3">
-                        <a class="inline-flex items-center gap-x-1 text-sm text-brand-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-brand-500" href="#">
-                          Edit
-                        </a>
-                        <a class="inline-flex items-center gap-x-1 text-sm text-red-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-red-500" href="#">
-                          Delete
-                        </a>
-                      </div>
-                    </td>
-                  </tr>
-                {:else}
-                  <tr>
-                    <td class="size-px whitespace-nowrap" colspan="4">
-                      <div class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
-                        <div class="flex items-center gap-x-3 text-center">
-                          <div class="grow">
-                            <span class="block text-xl text-gray-600 dark:text-neutral-200">NO CARDS CREATED</span>
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                {/each}
-              </tbody>
-            </table>
-
-            <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
-              <div>
-                <p class="text-sm text-gray-600 dark:text-neutral-400">
-                  <span class="font-semibold text-gray-800 dark:text-neutral-200">12</span> results
-                </p>
-              </div>
-
-              <div>
-                <div class="inline-flex gap-x-2">
-                  <a href={meta.prev_page_url || '#'} class="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
-                    <SvgIcon class="shrink-0 size-4" slot={leftAngle}/>
-                    Prev
-                  </a>
-
-                  <a href="{meta.next_page_url || '#'}" class="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
-                    Next
-                    <SvgIcon class="shrink-0 size-4" slot={rightAngle}/>
-                  </a>
-                </div>
-              </div>
-            </div>
+            <ProductListings products={cards} {meta} basePageUrl="/admin/gift-card"/>
           </div>
         </div>
       </div>
---
+++ src/routes/(admin)/admin/gift-card/[slug]/+page.server.js
@@ -0,0 +1,32 @@
+import { api } from '$lib/helpers';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load(event) {
+
+  console.log(event.params);
+
+
+  const fetchProducts = async () => {
+    const res = await api({
+			method: 'get',
+			resource: 'products/type/gift-card?cursor=' + event.params.slug,
+      event,
+		});
+
+    return res?.json();
+  }
+
+  event.setHeaders({
+    'Cache-Control': 'public, max-age=3600, stale-while-revalidate=86400',
+  });
+
+	const [productsData] = await Promise.all([
+    fetchProducts(),
+	]);
+
+  return {
+    /** @type { import('$lib/types').Product[] } */
+    products: productsData.data,
+    meta: productsData.metadata,
+  }
+}
