f7b5f8a1bdf19580535d48d6d2a193722be15931
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-11-05 11:52:56 +0100

    Implement page navigation

--- src/routes/(admin)/admin/product/[slug]/+page.server.js
+++ src/routes/(admin)/admin/product/[slug]/+page.server.js
@@ -3,23 +3,26 @@ import { api } from '$lib/helpers';
 /** @type {import('./$types').PageServerLoad} */
 export async function load(event) {
 
+  console.log(event.params);
+
+
   const fetchProducts = async () => {
     const res = await api({
 			method: 'get',
-			resource: 'products',
+			resource: 'products?cursor=' + event.params.slug,
       event,
 		});
 
     return res?.json();
   }
 
-	const [productsData] = await Promise.all([
-    fetchProducts(),
-	]);
-
   event.setHeaders({
     'Cache-Control': 'public, max-age=3600, stale-while-revalidate=86400',
   });
+
+	const [productsData] = await Promise.all([
+    fetchProducts(),
+	]);
 
   return {
     /** @type { import('$lib/types').Product[] } */
