4092c5b7494568afe0e9f08e3802c562ce377c8a
Ranger <oghale.b.emeselue@gmail.com>
2024-12-02 00:16:17 +0100

    Added edit action to admin gift card route, updated create

--- src/routes/(admin)/admin/gift-card/[slug]/+page.server.js
+++ src/routes/(admin)/admin/gift-card/[slug]/+page.server.js
@@ -85,6 +85,56 @@ export async function load(event) {
 
 /** @satisfies {import('./$types').Actions} */
 export const actions = {
+  edit: async (event) => {
+    const form = await superValidate(event, arktype(GiftCardSchema, { defaults: GiftCardDefaults }));
+
+    if (!form.valid) {
+      return fail(422, { form });
+    }
+
+    const formData = new FormData();
+
+    for(let dt of Object.entries(form.data)){
+      if (dt[0] == 'discount_until' && dt[1]) {
+        formData.append(dt[0], new Date(dt[1]).toDateString());
+        continue;
+      }
+
+      formData.append(dt[0], dt[1]);
+    }
+
+    const res = await api({
+			method: 'PUT',
+			resource: 'products/'+event.params.slug,
+			data: form.data,
+      event,
+      toJSON: false,
+		});
+
+    if (res?.status == 422) {
+      let errRes = await res.json();
+
+      for(const [fieldName, errs] of Object.entries(errRes.errors)){
+        if (fieldName.includes('.')) {
+          setError(form, fieldName.split('.')[0], errs[0], {
+            overwrite: true
+          });
+        } else {
+          setError(form, fieldName, errs[0], {
+            overwrite: true
+          });
+        }
+      }
+
+      return message(form, {type: 'error', msg: 'There are errors in your form! Check them and try again.'}, {status: res?.status || 400});
+		}
+
+    if ( ! res?.ok) {
+      return message(form, {type: 'error', msg: res?.statusText || 'An error occurred while processing your request'}, {status: res?.status || 429});
+    }
+
+		return message(form, {type: 'success', msg: 'Product Updated successfully!'});
+	},
   /** @param {import('@sveltejs/kit').RequestEvent} event */
   createBrand: async (event) => {
     const form = await superValidate(event, arktype(brandSchema, { defaults: brandDefaults }));
@@ -178,7 +228,7 @@ export const actions = {
 		}
 
     if ( ! res?.ok) {
-      return message(form, {type: 'error', msg: res?.statusText || 'An error occured while processing your request'}, {status: res?.status || 429});
+      return message(form, {type: 'error', msg: res?.statusText || 'An error occurred while processing your request'}, {status: res?.status || 429});
     }
 
 		return message(form, {type: 'success', msg: 'Brand was Updated successfully!'});
@@ -214,9 +264,9 @@ export const actions = {
 		}
 
     if ( ! res?.ok) {
-      return message(form, {type: 'error', msg: res?.statusText || 'An error occured while processing your request'}, {status: res?.status || 429});
+      return message(form, {type: 'error', msg: res?.statusText || 'An error occurred while processing your request'}, {status: res?.status || 429});
     }
 
-    return message(form, {type: 'success', msg: 'Brand was Updated successfully!'});
+    return message(form, {type: 'success', msg: 'Brand deleted successfully!'});
   },
 }
