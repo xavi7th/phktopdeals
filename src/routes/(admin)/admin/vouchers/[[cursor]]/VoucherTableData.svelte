f35cffd5bf8b7892068254f3affa1451832a4d8b
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-12-11 09:17:54 +0100

    Feat: Admin Preview Voucher emails

--- src/routes/(admin)/admin/vouchers/[[cursor]]/VoucherTableData.svelte
+++ src/routes/(admin)/admin/vouchers/[[cursor]]/VoucherTableData.svelte
@@ -131,6 +131,13 @@
                   on:click={() => dispatch("edit", voucher)}>
                   Edit Voucher
                 </button>
+
+                <a
+                  href="/admin/vouchers/email-preview/{voucher.id}"
+                  class="flex w-full items-center gap-x-3 rounded-lg px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
+                  on:click={() => dispatch("edit", voucher)}>
+                  Preview Email
+                </a>
               </div>
 
               <div class="py-2 first:pt-0 last:pb-0">
