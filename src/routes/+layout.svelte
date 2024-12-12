9b50e0de74b888af3f2adece49b006d3e1473842
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-12-12 08:21:49 +0100

    Remove noindex robot meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,19 +20,19 @@
 </script>
 
 <svelte:head>
-  <title>{($page.data?.seo?.title || "Welcome") + " | PHKHotDeals"}</title>
+  <title>{($page.data?.seo?.title || "Welcome") + " | PHK Hot Deals"}</title>
   <meta
     name="description"
     content={$page.data?.seo?.description ||
-      "Experience a groundbreaking e-commerce revolution with PHKHotDeals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHKHotDeals and transform the way you shop online."} />
+      "Experience a groundbreaking e-commerce revolution with PHK Hot Deals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHK Hot Deals and transform the way you shop online."} />
 
   <meta
     name="abstract"
     content={$page.data?.seo?.description ||
-      "Experience a groundbreaking e-commerce revolution with PHKHotDeals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHKHotDeals and transform the way you shop online."} />
+      "Experience a groundbreaking e-commerce revolution with PHK Hot Deals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHK Hot Deals and transform the way you shop online."} />
   <meta name="author" content="Ehikioya Daniel Akhile" />
 
-  <meta itemprop="name" content={$page.data?.seo?.title || "PHKHotDeals Ltd"} />
+  <meta itemprop="name" content={$page.data?.seo?.title || "PHK Hot Deals Ltd"} />
   <link itemprop="url" href={$page.data?.seo?.url || "https://www.phkhotdeals.com/"} />
   <meta name="theme-color" content="#facc15" />
   <meta name="apple-mobile-web-app-status-bar-style" content="#facc15" />
@@ -40,9 +40,9 @@
   <meta
     property="og:description"
     content={$page.data?.seo?.description ||
-      "Experience a groundbreaking e-commerce revolution with PHKHotDeals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHKHotDeals and transform the way you shop online."} />
-  <meta property="og:title" content={$page.data?.seo?.title || "PHKHotDeals Ltd"} />
-  <meta property="og:site_name" content="PHKHotDeals" />
+      "Experience a groundbreaking e-commerce revolution with PHK Hot Deals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHK Hot Deals and transform the way you shop online."} />
+  <meta property="og:title" content={$page.data?.seo?.title || "PHK Hot Deals Ltd"} />
+  <meta property="og:site_name" content="PHK Hot Deals" />
   <meta property="og:url" content={$page.data?.seo?.url || "https://www.phkhotdeals.com/"} />
   <meta property="og:image" content={$page.data?.seo?.imgUrl || "/favicon.png"} />
   <meta property="og:type" content={$page.data?.seo?.pageType || "website"} />
@@ -54,11 +54,11 @@
   <meta
     name="twitter:description"
     content={$page.data?.seo?.description ||
-      "Experience a groundbreaking e-commerce revolution with PHKHotDeals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHKHotDeals and transform the way you shop online."} />
-  <meta name="twitter:title" content={$page.data?.seo?.title || "PHKHotDeals Ltd"} />
+      "Experience a groundbreaking e-commerce revolution with PHK Hot Deals! Our blockchain-powered platform seamlessly integrates cryptocurrency transactions, offering over 190,000 virtual items worldwide. Step into the future of online shopping with instant delivery, support for 90+ cryptocurrencies, and strategic partnerships. Join PHK Hot Deals and transform the way you shop online."} />
+  <meta name="twitter:title" content={$page.data?.seo?.title || "PHK Hot Deals Ltd"} />
   <meta name="twitter:image" content={$page.data?.seo?.imgUrl || "/favicon.png"} />
 
-  <meta name="robots" content="noindex,nofollow" />
+  <meta name="robots" content="index,follow" />
 </svelte:head>
 
 <slot></slot>
