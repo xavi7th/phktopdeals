bd9abfef0d452981d468fe774d68bc92982ca418
Akhile E. Daniel <ehi.akhile@gmail.com>
2025-05-13 14:08:46 +0100

    Update footer

--- src/partials/Footer.svelte
+++ src/partials/Footer.svelte
@@ -29,7 +29,7 @@
           Providing you the best deals in the <a class="font-medium text-brand-600 decoration-2 hover:underline focus:underline focus:outline-none dark:text-brand-500" href="#/">online</a>
           space.
         </p>
-        <p class="text-gray-500 dark:text-neutral-500">© Minds&Machines {new Date().getFullYear()}. All rights reserved.</p>
+        <p class="text-gray-500 dark:text-neutral-500">© MOZYFLEX LTD Nigeria (RC 1577964). All rights reserved.</p>
       </div>
 
       <div class="mb-8 mt-3 space-x-2">
